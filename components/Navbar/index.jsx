8b40bf4ad06e256f4d5f3c47da72ae394a5c61a5
Anderson Nunes <dande.nunes.94@gmail.com>
2022-10-13 16:27:32 -0300

    aplicação de estilos e a logo d SouJunior ao projeto - pt.2

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Link from 'next/link'
 import {dataNavbar} from '../../utils/dataNavbar'
+import styles from "./Navbar.module.css"
 
 export const Navbar = () => {
   return (
-    <ul>
+    <ul className={styles.menuNav}>
     {dataNavbar.map((link,index)=>(
-      <li key={index}>
+      <li className={styles.linkNav} key={index}>
         <Link href={link.path}>
           {link.title}
         </Link>
