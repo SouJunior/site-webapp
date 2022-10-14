423db6d666b1ef6ed6a8c5409a931ab045fc9ce6
[edcabralc] <[cabralnet@gmail.com]>
2022-10-14 09:32:39 -0300

    feat: Add section Ocuppation Area

---
+++ components/OccupationSection/index.jsx
@@ -0,0 +1,33 @@
+import styles from "../OccupationSection/OccupationSection.module.css";
+
+export const OccupationSection = () => {
+  const item = [
+    {title: "Teste", src: "https://via.placeholder.com/150"},
+    {title: "Teste", src: "https://via.placeholder.com/150"},
+    {title: "Teste", src: "https://via.placeholder.com/150"},
+    {title: "Teste", src: "https://via.placeholder.com/150"},
+    {title: "Teste", src: "https://via.placeholder.com/150"},
+    {title: "Teste", src: "https://via.placeholder.com/150"},
+    {title: "Teste", src: "https://via.placeholder.com/150"},
+    {title: "Teste", src: "https://via.placeholder.com/150"},
+  ];
+
+  return (
+    <section className={styles.OccupationSection}>
+      <div className={styles.OccupationContainer}>
+        <h2 className={styles.titlePrimary}>Áreas de atuação</h2>
+        <p className={styles.paragraph}>
+          Aqui no SouJunior, atuam profissionais iniciantes das diversas áreas
+          que compõem uma empresa de tecnologia. Confira abaixo as áreas de
+          atuação que temos em nosso quadro atualmente:
+        </p>
+
+        <div className={styles.itemContainer}>
+          {item.map(({title, src}) => (
+            <img src={src} alt={title} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
+import {OccupationSection} from "../components/OccupationSection";
 
 export default function Home() {
   return (
     <main>
       <h1>Página principal</h1>
+      <OccupationSection />
     </main>
-  )
+  );
 }
