ad7d9a3f0fed12204685ffb06b7db4c45126dfce
anikape <ana.br@hotmail.com>
2022-10-25 10:35:47 -0300

    Ajustes em ParticipationSection

--- components/Participation/ParticipationSection.jsx
+++ components/Participation/ParticipationSection.jsx
@@ -20,7 +20,7 @@ return (
         <h1 className={styles.h1}>
         Quem pode participar?
                 </h1>
-        <p> Temos oportunidades para profissionais Juniors e mentores de diversas profissões.
+        <p className={styles.p}> Temos oportunidades para profissionais Juniors e mentores de diversas profissões.
              Além disso, temos oportunidades para apoiadores e parceiros para diferentes
               frentes de contribuição para o projeto.
              Veja ao lado quais são as categorias de participantes disponíveis para
@@ -37,7 +37,7 @@ return (
     textJr &&
 
     <>
-    <p>São as pessoas que querem trabalhar na área de tecnologia, estudam ou estão estudando
+    <p className={styles.p}>São as pessoas que querem trabalhar na área de tecnologia, estudam ou estão estudando
     sobre a área de interesse, mas <b>ainda não tem toda experiência exigida</b> pelos
     recrutadores e empresas para contratarem para uma vaga júnior.<br/>
     
@@ -51,23 +51,25 @@ return (
     Daremos sempre prioridade para aqueles que já participarem do servidor <b>SouJunior no Discor</b>
     e do <b>Github do SouJunior</b> por já estarem familiarizados com as ferramentas e sobre como o projeto funciona.
     </p><br/>
-<a href="https://forms.gle/d7yFWXjCPj9CLFFN9">Quero Participar</a>
+    <button className={styles.button} ><a href="https://forms.gle/d7yFWXjCPj9CLFFN9"> Quero Participar</a></button>
 </>
+
 }
 
         </div>
+
         <h3 className={styles.border}> <button type="button" className={styles.btn} onClick={()=> setTextMentor(!textMentor)}><IoMdAdd /></button> Sou Mentor</h3>
         <div className={styles.textMentor}>
         {
 
 textMentor &&
 <>
-<p>
+<p className={styles.p}>
     São profissionais já inseridos e <b>atuantes no mercado de trabalho, há pelo menos 6 meses.</b><br/>
     Se você tem disponibilidade e interesse em contribuir para o SouJunior, preencha o formulário de inscrição quere
     entraremos em contato quando tivermos demanda de pessoas mentoras na área de atuação.<br/>
 </p><br/>
-<a href="https://forms.gle/d7yFWXjCPj9CLFFN9">Quero Participar</a>
+<button className={styles.button} ><a href="https://forms.gle/d7yFWXjCPj9CLFFN9"> Quero Participar</a></button>
 </>
 }
 </div> 
@@ -79,7 +81,7 @@ textMentor &&
     textSupporter &&
 
     <>
-    <p> São pessoas que tenham interesse e possam contribuir de alguma forma para o projeto SouJunior.<br/>
+    <p className={styles.p}> São pessoas que tenham interesse e possam contribuir de alguma forma para o projeto SouJunior.<br/>
     <VscCircleFilled /> Pessoas de divulgação, que tenham canal no YouTube, podcast, Blog ou Administradores de grupos, todos que abordem assuntos
     referentes às áreas de atuação de profissionais de tecnologia.<br/>
     <VscCircleFilled /> Pessoas especialistas que se disponham em <b>palestrar sobre algum assunto de interesse,</b>aos profissionais Juniors de tecnologia. <br/>
@@ -87,35 +89,39 @@ textMentor &&
     Se você pode contribuir para o SouJunior como pessoa apoiadora ou parceira, inscreva-se através do formulário e nossa equipe
     responsável entrará em contato com você.
     </p><br/>
-<a href="https://forms.gle/d7yFWXjCPj9CLFFN9">Quero Participar</a>
+    <br/>
+    <button className={styles.button} ><a href="https://forms.gle/d7yFWXjCPj9CLFFN9"> Quero Participar</a></button>
 </>
 }   
 </div>   
 
+<h3 className={styles.border}> <button type="button" className={styles.btn} onClick={()=> setTextCommunity(!textCommunity)}><IoMdAdd /></button> Comunidade</h3>
+<div className={styles.textCommunity}>
 
-    <h3 className={styles.border}> <button type="button" className={styles.btn} onClick={()=> setTextCommunity(!textCommunity)}><IoMdAdd /></button> Comunidade</h3>    
-
-    <div className={styles.textCommunity}>
 {
-
-textCommunity &&
+    textCommunity &&
 
     <>
-    <p>
-        São pessoas que queiram saber como funciona e o que está acontecendo no projeto. <br/>
-        Como o SouJunior é um plataforma Open Source, qualquer pessoa que se interesse pode acessar as informações
-        sobre o andamento do projeto. <br/>
-        Desde <b>curiosos</b> que só querem dar uma olhadinha, até aqueles que queiram <b>observar como o SouJunior funciona</b>, antes de 
-        decidirem se querem participar do projeto ou não. <br/>
-        Acesse o servidor <b>SouJunior no Discord</b> e do <b>Github do SouJunior</b> espie e acompanhe o dia a dia do projeto,
+    
+    <p className={styles.p}>
+    São pessoas que queiram saber como funciona e o que está acontecendo no projeto. <br/>
+    Como o SouJunior é um plataforma Open Source, qualquer pessoa que se interesse pode acessar as informações
+    sobre o andamento do projeto. <br/> 
+    Desde <b>curiosos</b> que só querem dar uma olhadinha, até aqueles que queiram <b>observar como o SouJunior funciona</b>, antes de 
+    decidirem se querem participar do projeto ou não. <br/>
+    Acesse o servidor <b>SouJunior no Discord</b> e do <b>Github do SouJunior</b> espie e acompanhe o dia a dia do projeto,
         inclusive como ouvite nas reuniões nos canais de voz. <br/>
-        Ao entrar no servidor SouJunior do Discord, leia atentamente as regras <br>#rules</br> e fique sempre atento ao
-        <b>#chat</b> para interagir com os demais participantes da comunidade.
-    </p><br/>
-<a href="https://forms.gle/d7yFWXjCPj9CLFFN9" target="_blank">Quero Participar</a>
-</>
-}   
-</div>   
+        Ao entrar no servidor SouJunior do Discord, leia atentamente as regras <> <b>#rules</b></> e fique sempre atento ao <><b>#chat</b></> para interagir com os demais participantes da comunidade.	
+       
+    </p> <br/>
+    <button className={styles.button} ><a href="https://forms.gle/d7yFWXjCPj9CLFFN9"> Quero Participar</a></button>    
+    </>
+
+
+}
+
+</div>
+
 
     </section>
 
