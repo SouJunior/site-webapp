13e4ef2269296ceee6bc573fe2b9541e4e6d1fb0
unknown <mihporto@hotmail.com>
2022-11-18 18:59:04 -0300

    atualizacao

--- components/AboutSection/index.jsx
+++ components/AboutSection/index.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import styles from "./AboutSection.module.css";
+import Image from "next/image";
+
+
 
 export const AboutSection = () => {
   return (
-    <section>
+    <section className={styles.container}>
       <div className={styles.alight}> 
       <h1 className={styles.title}> O que é o Sou Junior?</h1>
       <p className={styles.text}>
@@ -18,9 +21,19 @@ export const AboutSection = () => {
         rede de apoio, além do networking e conexão com novas
         oportunidades.
         </p>
-      </div>
+        </div>
+
+        
+        <div className={styles.galaxia}>
+
+           {/* identificar o erro no comando abaixo */}
+          {/* <Image src="/assets/galaxia.png" alt="galaxia" width={410} heigth={470}/> */}
+          <img src="../../assets/galaxia.png" alt="galaxia"/>
+          </div>
     </section>
+
   );
 };
+
 
      
