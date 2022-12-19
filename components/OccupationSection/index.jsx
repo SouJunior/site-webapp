729707b0e608b66fada3024b5635aa1c6ed570e2
edcabralc <cabralnet@gmail.com>
2022-12-19 20:35:23 -0300

    fix: add touch feature in carousel

--- components/OccupationSection/index.jsx
+++ components/OccupationSection/index.jsx
@@ -3,16 +3,18 @@ import Image from "next/image";
 import styles from "../OccupationSection/OccupationSection.module.css";
 import {carouselItems} from "../../utils/carouselItems";
 
+
 export const OccupationSection = () => {
-  const carrousel = useRef(null);
+  const carousel = useRef(null);
 
   const handleLeftSide = (e) => {
     e.preventDefault();
-    carrousel.current.scrollLeft -= carrousel.current.offsetWidth + 159;
+    carousel.current.scrollLeft -= carousel.current.clientWidth;
+    
   };
   const handleRightSide = (e) => {
     e.preventDefault();
-    carrousel.current.scrollLeft += carrousel.current.offsetWidth + 159;
+    carousel.current.scrollLeft += carousel.current.clientWidth;    
   };
 
   return (
@@ -20,12 +22,12 @@ export const OccupationSection = () => {
       <div className={styles.OccupationContainer}>
         <h2 className={styles.titlePrimary}>Áreas de atuação</h2>
         <p className={styles.paragraph}>
-          Aqui na SouJunior, atuam profissionais iniciantes das diversas áreas
+          Aqui no SouJunior, atuam profissionais iniciantes das diversas áreas
           que compõem uma empresa de tecnologia. Confira abaixo as áreas de
           atuação que temos em nosso quadro atualmente:
         </p>
-
-        <div className={styles.itemContainer} ref={carrousel}>
+     
+        <div className={styles.itemContainer} ref={carousel}>
           {carouselItems.map(({title, icon}) => (
             <div key={title} className={styles.item}>
               <Image src={icon} alt={title} width={61} height={61} />
@@ -33,6 +35,7 @@ export const OccupationSection = () => {
             </div>
           ))}
         </div>
+      
         <div className={styles.arrowContainer}>
           <button onClick={handleLeftSide}>
             <Image
