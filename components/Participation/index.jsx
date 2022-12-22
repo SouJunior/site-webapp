d87f7c8e3b985760760685a6960dece02b49fa0b
anikape <ana.br@hotmail.com>
2022-12-22 10:36:48 -0300

    correção de texto

--- components/Participation/index.jsx
+++ components/Participation/index.jsx
@@ -38,7 +38,7 @@ export const ParticipationSection = () => {
             <br />
             Porém, se o interesse for de patrocinar a iniciativa para que ela
             continue por muito tempo, você poderá se tornar nosso apoiador ou
-            parceiro. Confira ao abaixo todas as categorias de participação
+            parceiro. Confira abaixo todas as categorias de participação
             disponíveis na SouJunior, preencha os formulários das quais for do
             seu interesse e entraremos em contato!
           </p>
