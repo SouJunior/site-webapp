305958fd06f6fc4cdb59b9bd238347a3500597af
wouerner <wouerner@protonmail.com>
2022-12-22 10:25:29 -0300

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,20 @@
+import Script from 'next/script'
 import "../styles/globals.css";
 import {Layout} from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-L5P4NG9T9Y"></Script>
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-L5P4NG9T9Y');
+        `}
+      </Script>
       <Component {...pageProps} />
     </Layout>
   )
