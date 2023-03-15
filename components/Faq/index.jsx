2b227f04f2decead12a686ea588c6f9100efcd3a
Ana Santos <ana.br@hotmail.com>
2023-03-15 08:07:31 -0300

    create datalists - faq

---
+++ components/Faq/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import {dataFaq} from "../../utils/dataSocial";
+
+export const Faq = () => {
+  return (
+    <>
+   TESTE
+    </>
+  );
+};
---
+++ utils/souJunior.js
@@ -0,0 +1,26 @@
+export const SouJunior = [
+  {
+    titulo: 'Qual o objetivo da SouJunior?',
+    descricao:
+      'A SouJunior tem como objetivo apoiar e preparar profissionais juniores para o mercado de tecnologia, através do voluntariado.',
+  },
+
+  {
+    titulo: 'Como fazer parte da Comunidade da SouJunior?',
+    descricao:
+      'Para você se conectar com a Comunidade da SouJunior, acesse o servidor SouJunior no Discord e no Github.Ao entrar no servidor SouJunior no Discord, leia as regras e fique sempre atento ao #chat para interagir com os demais participantes da Comunidade.Para fazer parte da Comunidade da SouJunior, selecione no Menu da Home do Site SouJunior a opção “Participar”, em seguida escolha a seção “Comunidade” e clique em “Quero Participar”. Você será direcionado para o formulário de candidatura que, após enviado, nossa equipe responsável entrará em contato com você.',
+  },
+
+  {
+    titulo: 'A SouJunior oferece alguma ajuda de custo para quem é voluntário?',
+    descricao:
+      'Não. Todos os voluntários da SouJunior atuam em regime home office.',
+  },
+
+  {
+    titulo:
+      'Qual auxílio a Sou Junior oferece ao junior em relação ao mercado de trabalho?',
+    descricao:
+      'Teremos, em breve, o nosso “Portal de vagas” onde divulgaremos as oportunidades e facilitaremos o contato dos juniores com as empresas através da divulgação do contato.',
+  },
+];
