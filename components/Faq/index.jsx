af56a4c8979708f95a177d4b2dd98a7dc4cce944
Edvaldo Cabral <cabralnet@gmail.com>
2023-04-17 15:15:28 -0300

    fix: uninstall libs for testing email

--- components/Faq/index.jsx
+++ components/Faq/index.jsx
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 import { useState } from "react";
-import emailjs from "@emailjs/browser";
 import { IoMdAdd, IoMdClose } from "react-icons/io";
 import { Accordion, AccordionItem } from "@szhsin/react-accordion";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
@@ -11,90 +9,14 @@ import Input from "../commons/Input";
 import Textarea from "../commons/Textarea";
 import { RadioButton } from "../commons/RadioButton";
 import { souJunior, mentor, voluntario } from "../../utils/faqItems";
-=======
-import React, { useState } from "react";
-import { IoMdAdd, IoMdClose } from "react-icons/io";
-import Title from "../commons/Title";
-import styles from "./Faq.module.css";
-import { useForm } from "react-hook-form";
-import emailjs from "@emailjs/browser";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-
-import { souJunior, mentor, voluntario } from "../../utils/faqItems";
-import { Accordion, AccordionItem } from "@szhsin/react-accordion";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
-import Input from "../commons/Input";
-import Textarea from "../commons/Textarea";
-import { RadioButton } from "../commons/RadioButton";
-
-//Validação do FORM
-const schema = yup.object().shape({
-  name: yup.string().required("'Nome é obrigatório'"),
-  email: yup
-    .string()
-    .email("'E-mail inválido'")
-    .required("E-mail é obrigatório"),
-  description: yup.string().required("'Mensagem é obrigatória'"),
-});
->>>>>>> 3bdc870b518d8690e6f5590bd7cfccdf2d930622
 
 export const Faq = () => {
-  //Validação do FORM
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  });
-  const onSubmit = (data) => console.log(data);
-
   const [souJr, setSouJr] = useState(false);
   const [icon, setIcon] = useState(IoMdAdd);
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [radioOption, setRadioOption] = useState("Sou Junior");
-
-  function sendEmail(e) {
-<<<<<<< HEAD
-    e.preventDefault();
-
-    const templateParams = {
-      from_name: name,
-      message: message,
-      email: email,
-    };
-
-    emailjs
-      .send(
-        "service_k47b2cj",
-        "template_a9xnen5",
-        templateParams,
-        "BeY4OuM8WvMaH_COp"
-      )
-      .then(
-        (response) => {
-          alert("E-mail enviado com sucesso!");
-          console.log("email enviado", response.status, response.text);
-          setName("");
-          setEmail("");
-          setMessage("");
-        },
-        (err) => {
-          console.log("Erro", err);
-        }
-      );
-=======
-    //  e.preventDefault();
-
-    alert("enviado");
-    setName("");
-    setEmail("");
-    setMessage("");
->>>>>>> 3bdc870b518d8690e6f5590bd7cfccdf2d930622
-  }
 
   return (
     <>
@@ -175,11 +97,7 @@ export const Faq = () => {
       </div>
 
       <section className={styles.formSection}>
-<<<<<<< HEAD
-        <form className={styles.form} onSubmit={sendEmail}>
-=======
-        <form className={styles.form} onSubmit={handleSubmit(sendEmail)}>
->>>>>>> 3bdc870b518d8690e6f5590bd7cfccdf2d930622
+        <form className={styles.form}>
           <div className={styles.radios}>
             <RadioButton
               options={[
@@ -191,31 +109,6 @@ export const Faq = () => {
               value={radioOption}
               setValue={setRadioOption}
             />
-<<<<<<< HEAD
-            <RadioButton
-              id="teste"
-              name="teste"
-              titleFor="teste"
-              title="Outro teste"
-            />
-            <div className={styles.radiosContainer}>
-              <input type="radio" name="soujunior" id="soujunior" />
-              <label htmlFor="soujunior">Sou Junior</label>
-            </div>
-            <div className={styles.radiosContainer}>
-              <input type="radio" name="soujunior" id="voluntario" />
-              <label htmlFor="voluntario">Voluntário</label>
-            </div>
-            <div className={styles.radiosContainer}>
-              <input type="radio" name="soujunior" id="mentor/apoiador" />
-              <label htmlFor="mentor/apoiador">Mentor/Apoiador</label>
-            </div>
-            <div className={styles.radiosContainer}>
-              <input type="radio" name="soujunior" id="outros" />
-              <label htmlFor="outros">Outros</label>
-            </div>
-=======
->>>>>>> 3bdc870b518d8690e6f5590bd7cfccdf2d930622
           </div>
 
           <div className={styles.labelInput}>
@@ -224,11 +117,7 @@ export const Faq = () => {
               text="Qual o seu nome?*"
               placeholder="Digite seu nome completo"
               name="name"
-              {...register("name")}
             />
-            {errors.name && (
-              <p className={styles.error}>{errors.name.message}</p>
-            )}
           </div>
 
           <div>
@@ -237,22 +126,11 @@ export const Faq = () => {
               text="Qual o seu e-mail?*"
               placeholder="Digite o seu e-mail"
               name="email"
-              {...register("email")}
             />
-            {errors.email && (
-              <p className={styles.error}>{errors.email.message}</p>
-            )}
           </div>
 
           <div className={styles.area}>
-            <Textarea
-              name="description"
-              text="Fale-nos sobre sua dúvida*"
-              {...register("description")}
-            />
-            {errors.description && (
-              <p className={styles.error}>{errors.description.message}</p>
-            )}
+            <Textarea name="description" text="Fale-nos sobre sua dúvida*" />
 
             <button className={styles.button} type="submit">
               Enviar
