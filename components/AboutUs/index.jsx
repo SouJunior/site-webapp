48fa24232393438e8e5b291a927e059c7f8e5be5
Edvaldo Cabral <cabralnet@gmail.com>
2023-04-28 13:28:17 -0300

    fix: estilos na página faq

--- components/AboutUs/index.jsx
+++ components/AboutUs/index.jsx
@@ -5,7 +5,6 @@ import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import Typography from "@mui/material/Typography";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { AiFillLinkedin } from "react-icons/ai";
 import { FaDiscord } from "react-icons/fa";
 import { headers } from "../../utils/headers";
