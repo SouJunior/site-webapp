0546e56ce71281e74d7c013fd718b584c89e4703
Ana Santos <ana.br@hotmail.com>
2023-05-18 10:26:30 -0300

    Estilização dos banners da home

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,7 +1,7 @@
 import { OccupationSection } from "../components/OccupationSection";
 import { AboutSection } from "../components/AboutSection";
 import { ParticipationSection } from "../components/Participation";
-import Iniciativas2 from "../components/Iniciativas2"
+import Iniciativas from "../components/Iniciativas"
 import HeaderComponent from "../components/commons/HeaderComponent/HerderC";
 
 const Home = () => {
@@ -10,7 +10,7 @@ const Home = () => {
       <HeaderComponent tittle={"Texto para a página home!"} />
       <AboutSection />
       <OccupationSection />
-      <Iniciativas2 />
+      <Iniciativas />
       <ParticipationSection />
     </>
   );
