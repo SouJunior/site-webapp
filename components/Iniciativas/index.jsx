868bcfbd901f5ec9b026e975f71cd9585af6415f
Edvaldo Cabral <cabralnet@gmail.com>
2023-05-31 13:25:38 -0300

    fix: add key prop in Swipe map

--- components/Iniciativas/index.jsx
+++ components/Iniciativas/index.jsx
@@ -6,47 +6,49 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import styles from './Iniciativa.module.css'
-import Card from '../commons/Card/Card';
-
-
+import styles from "./Iniciativa.module.css";
+import Card from "../commons/Card/Card";
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper";
 
-
 const cardData = [
-  { id: 1,
-    title: 'Conheça outras iniciativas da SouJunior',
-    subtitle:'SouJunior Lab',
-    description:'Coloque em pratica suas expertises em projetos reais, e ganhe experiência no mercado de trabalho.',
-    image: 'assets/iniciativa/lab.jpg',
-    type: 'type-1',
-   },
-  { id: 2,
-    title: 'Conheça outras iniciativas da SouJunior',
-    subtitle:'SouJunior Talk',
-    description:'Se você está procurando uma maneira de aprimorar seu inglês com pessoas reais, temos uma excelente notícia para você!A SouJunior TAlk é um ambiente de treino de inglês que oferece uma plataforma interativa onde você pode praticar suas habilidades linguísticas. ',
-    image: 'assets/iniciativa/talk.svg',
-    type: 'type-2' 
-   },
   {
-    id:3,
-    title: 'Conheça outras iniciativas da SouJunior',
+    id: 1,
+    title: "Conheça outras iniciativas da SouJunior",
     subtitle: "SouJunior Lab",
-    description:"Coloque em pratica suas expertises em projetos reais, e ganhe experiência no mercado de trabalho.",
-    image: 'assets/iniciativa/lab.jpg',  
-    type: 'type-1' 
+    description:
+      "Coloque em pratica suas expertises em projetos reais, e ganhe experiência no mercado de trabalho.",
+    image: "assets/iniciativa/lab.jpg",
+    type: "type-1",
+  },
+  {
+    id: 2,
+    title: "Conheça outras iniciativas da SouJunior",
+    subtitle: "SouJunior Talk",
+    description:
+      "Se você está procurando uma maneira de aprimorar seu inglês com pessoas reais, temos uma excelente notícia para você!A SouJunior TAlk é um ambiente de treino de inglês que oferece uma plataforma interativa onde você pode praticar suas habilidades linguísticas. ",
+    image: "assets/iniciativa/talk.svg",
+    type: "type-2",
+  },
+  {
+    id: 3,
+    title: "Conheça outras iniciativas da SouJunior",
+    subtitle: "SouJunior Lab",
+    description:
+      "Coloque em pratica suas expertises em projetos reais, e ganhe experiência no mercado de trabalho.",
+    image: "assets/iniciativa/lab.jpg",
+    type: "type-1",
   },
   {
     id: 4,
-    title: 'Conheça outras iniciativas da SouJunior',
-    subtitle:'SouJunior Talk',
-    description:'Se você está procurando uma maneira de aprimorar seu inglês com pessoas reais, temos uma excelente notícia para você!A SouJunior TAlk é um ambiente de treino de inglês que oferece uma plataforma interativa onde você pode praticar suas habilidades linguísticas. ',
-    image: 'assets/iniciativa/talk.svg', 
-    type: 'type-2' 
-  }
-
+    title: "Conheça outras iniciativas da SouJunior",
+    subtitle: "SouJunior Talk",
+    description:
+      "Se você está procurando uma maneira de aprimorar seu inglês com pessoas reais, temos uma excelente notícia para você!A SouJunior TAlk é um ambiente de treino de inglês que oferece uma plataforma interativa onde você pode praticar suas habilidades linguísticas. ",
+    image: "assets/iniciativa/talk.svg",
+    type: "type-2",
+  },
 ];
 
 export default function App() {
@@ -58,13 +60,12 @@ export default function App() {
           delay: 7000,
           disableOnInteraction: false,
         }}
-       
         pagination={{
           clickable: true,
         }}
         modules={[Autoplay, Pagination]}>
-        {cardData.map(({ title, subtitle, description, image, type }) => (
-          <SwiperSlide>
+        {cardData.map(({ title, subtitle, description, image, type }, id) => (
+          <SwiperSlide key={id}>
             <Card
               type={type}
               title={title}
