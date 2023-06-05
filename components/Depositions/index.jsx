e487a1702a40dfdf96cf2a02c09f343fd014c835
Lucas Victor de Souza Dantas <68877260+lucasmbrute2@users.noreply.github.com>
2023-06-05 16:05:20 -0300

    feat: add collaborator carousel

--- components/Depositions/index.jsx
+++ components/Depositions/index.jsx
@@ -3,8 +3,22 @@ import { data } from "../../utils/depositionCarouselInfo"
 import Image from "next/image"
 import { DepositionCard } from "../commons/DepositionCard"
 import 'pure-react-carousel/dist/react-carousel.es.css';
+import { Swiper, SwiperSlide } from "swiper/react"
+import { useRef } from "react";
+import arrowLeft from "../../public/assets/arrow-left.svg"
+import arrowright from "../../public/assets/arrow-right.svg"
 
 export function Depositions() {
+  const carousel = useRef(null);
+
+  const handleLeftSide = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.clientWidth;
+  };
+  const handleRightSide = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.clientWidth;
+  };
   return <section className={style.depositionsSectionWrapper}>
     <header className={style.depositionsHeader}>
       <h2 className={style.depositionsTitle}>
@@ -24,8 +38,26 @@ export function Depositions() {
         </div>
         <p className={style.subTitle}>O que dizem sobre nós</p>
         </div>
-      <div className={style.carouselContainer}>
-        <DepositionCard imgPath={data[0].imgPath} title={'collaborator.title'} description={'collaborator.description'} role={'role'} name={'Priscila Souza'}/>
+      
+      <div className={style.carouselWrapper}>
+        <Swiper 
+          ref={carousel}
+          className={style.carouselContainer}   
+          spaceBetween={50}
+          slidesPerView={1}
+        >
+          {data.map(collaborator => (
+            <SwiperSlide key={collaborator.imgPath}>
+              <DepositionCard imgPath={collaborator.imgPath} title={collaborator.title} description={collaborator.description} role={collaborator.role} name={collaborator.name}/>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+          <span onClick={handleLeftSide} className={style.leftArrow}>
+            <Image src={arrowLeft}/>
+          </span>
+          <span onClick={handleRightSide} className={style.rightArrow}>
+            <Image src={arrowright}/>
+          </span>
       </div>
     </div>
   </section>
