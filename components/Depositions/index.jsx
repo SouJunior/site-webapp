73f1b04ce3672a88329ca88dabd5ef8dda04e984
Edvaldo Cabral <cabralnet@gmail.com>
2023-06-20 14:48:23 -0300

    fix: correção no carousel de depoimentos

--- components/Depositions/index.jsx
+++ components/Depositions/index.jsx
@@ -1,66 +1,66 @@
-import style from "./Depositions.module.css"
-import { data } from "../../utils/depositionCarouselInfo"
-import Image from "next/image"
-import { DepositionCard } from "../commons/DepositionCard"
-import 'pure-react-carousel/dist/react-carousel.es.css';
-import { Swiper, SwiperSlide } from "swiper/react"
-import { Navigation } from "swiper";
-import { useRef } from "react";
-import arrowLeft from "../../public/assets/arrow-left.svg"
-import arrowright from "../../public/assets/arrow-right.svg"
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Controller, Navigation } from "swiper";
+import { DepositionCard } from "../commons/DepositionCard";
+import style from "./Depositions.module.css";
+import { data } from "../../utils/depositionCarouselInfo";
 
 export function Depositions() {
-  const carousel = useRef(null);
+  return (
+    <>
+      <header className={style.depositionsHeader}>
+        <h2 className={style.depositionsTitle} id="depoimentos">
+          Depoimentos
+        </h2>
+      </header>
 
-  const handleLeftSide = (e) => {
-    e.preventDefault();
-    carousel.current.scrollLeft -= carousel.current.clientWidth;
-  };
-  const handleRightSide = (e) => {
-    e.preventDefault();
-    carousel.current.scrollLeft += carousel.current.clientWidth;
-  };
-  return <section className={style.depositionsSectionWrapper}>
-    <header className={style.depositionsHeader}>
-      <h2 className={style.depositionsTitle}>
-        Depoimentos
-      </h2>
-    </header>
-
-    <div className={style.depositionsContainer}>
-      <div>
-        <div className={style.depositionsContainerWrapper}>
-          {data.map(collaborator => (
-            <div key={collaborator.imgPath}>
-              <Image src={collaborator.imgPath} alt='' width={80} height={80} className={style.collaboratorImg}/>
+      <section className={style.DepositionsSection}>
+        <div className={style.container}>
+          <div className={style.wrapper}>
+            <div className={style.depositionsCollaborators}>
+              <div className={style.collaborator}>
+                {data
+                  .filter((collaborator) => collaborator.id <= 7)
+                  .map(({ imgPath }) => (
+                    <div className={style.collaboratorImg}>
+                      <Image
+                        key={imgPath}
+                        src={imgPath}
+                        alt=""
+                        width={90}
+                        height={90}
+                        className={style.collaboratorImg}
+                      />
+                    </div>
+                  ))}
+              </div>
+              <p className={style.subTitle}>O que dizem sobre nós</p>
             </div>
-          ))}
 
+            <div className={style.depositionsCards}>
+              <Swiper
+                className={style.cardWrapper}
+                spaceBetween={60}
+                slidesPerView={1}
+                navigation={true}
+                controller={true}
+                modules={[Controller, Navigation]}>
+                {data.map(({ imgPath, title, description, role, name }) => (
+                  <SwiperSlide key={imgPath}>
+                    <DepositionCard
+                      imgPath={imgPath}
+                      title={title}
+                      description={description}
+                      role={role}
+                      name={name}
+                    />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </div>
         </div>
-        <p className={style.subTitle}>O que dizem sobre nós</p>
-        </div>
-       
-      
-      <div className={style.carouselWrapper}>
-        <Swiper 
-          ref={carousel}
-          className={style.carouselContainer}   
-          spaceBetween={50}
-          slidesPerView={1}
-        >
-          {data.map(collaborator => (
-            <SwiperSlide key={collaborator.imgPath}>
-              <DepositionCard imgPath={collaborator.imgPath} title={collaborator.title} description={collaborator.description} role={collaborator.role} name={collaborator.name}/>
-            </SwiperSlide>
-          ))} 
-        </Swiper>
-          <span onClick={handleLeftSide} className={style.leftArrow}>
-            <Image src={arrowLeft}/>
-          </span>
-          <span onClick={handleRightSide} className={style.rightArrow}>
-            <Image src={arrowright}/>
-          </span>
-      </div>
-    </div>
-  </section>
+      </section>
+    </>
+  );
 }
