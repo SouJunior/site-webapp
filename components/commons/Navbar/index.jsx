88fb5f7550f2be5893898bf4a9583ed60fe1ef5b
Edvaldo Cabral <cabralnet@gmail.com>
2023-06-22 11:31:38 -0300

    feature: novo menu adicionado ao projeto

--- components/commons/Navbar/index.jsx
+++ components/commons/Navbar/index.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import { dataNavbar } from "../../../utils/dataNavbar";
 import styles from "./Navbar.module.css";
 
 export const Navbar = () => {
+  const router = useRouter();
+
   return (
     <ul className={styles.menuNav}>
       {dataNavbar.map((link, index) => (
         <li className={styles.linkNav} key={index}>
-          <Link href={link.path}>{link.title}</Link>
+          <Link href={link.path} passHref>
+            <a className={router.pathname === link.path ? styles.active : ""}>
+              {link.title}
+            </a>
+          </Link>
         </li>
       ))}
     </ul>
