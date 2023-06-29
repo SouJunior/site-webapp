910d0884d9c38e1a9726ec0fda4d2eb4d9f8c617
Edvaldo Cabral <cabralnet@gmail.com>
2023-06-29 14:33:58 -0300

    fix: responsividade e posicionamento footer

--- pages/faq.jsx
+++ pages/faq.jsx
@@ -1,6 +1,4 @@
 import { Faq } from "../components/Faq";
-import FooterV2 from "../components/commons/FooterV2";
-import HeaderComponent from "../components/commons/HeaderComponent";
 
 const faq = () => <Faq />;
 
