e8cc3b069781b5331457421f3017df4e213b0a73
Edvaldo Cabral <cabralnet@gmail.com>
2023-06-29 10:48:01 -0300

    fix: alteração dos itens de texto

--- components/commons/Popup/Popup.jsx
+++ components/commons/Popup/Popup.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styles from "./Popup.module.css";
+import { Paragraph } from "../Paragraph";
 
 const Popup = ({ message, onClose }) => {
   return (
     <div className={styles.overlay}>
       <div className={styles.popup}>
-        <p>{message}</p>
-
+        <Paragraph>{message}</Paragraph>
         <img src="/assets/popup.svg" alt="Imagem de sucesso" />
-       
+
         <button onClick={onClose}>Fechar</button>
       </div>
     </div>
