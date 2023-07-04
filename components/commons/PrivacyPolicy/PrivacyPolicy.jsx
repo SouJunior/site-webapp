3d5bf151944881611f79349cecb8f3fd311ed4c1
Ana Santos <ana.br@hotmail.com>
2023-07-03 21:09:24 -0300

    Criação dos cookies

--- components/commons/PrivacyPolicy/PrivacyPolicy.jsx
+++ components/commons/PrivacyPolicy/PrivacyPolicy.jsx
@@ -1,12 +1,40 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import style from "./PrivacyPolicy.module.css";
 
 const PrivacyPolicy = () => {
   const [accepted, setAccepted] = useState(false);
 
   const handleAccept = () => {
+    setCookie("privacy_accepted", true, 7); // Define o cookie de aceite de privacidade com valor "true" e expiração de 7 dias
     setAccepted(true);
   };
+
+  useEffect(() => {
+    const privacyAccepted = getCookie("privacy_accepted");
+    if (privacyAccepted) {
+      setAccepted(true);
+    }
+  }, []);
+
+  // Função para definir um cookie
+  const setCookie = (name, value, days) => {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+  };
+
+  // Função para obter o valor de um cookie
+  const getCookie = (name) => {
+    const cookies = document.cookie.split("; ");
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i].split("=");
+      if (cookie[0] === name) {
+        return cookie[1];
+      }
+    }
+    return null;
+  };
+  
 
   return (
     <div>
