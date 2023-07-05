5e2c47d422295171deea5c076902923f322c4a62
Edvaldo Cabral <cabralnet@gmail.com>
2023-07-05 12:20:14 -0300

    styles: correção size das fontes

--- components/Depositions/index.jsx
+++ components/Depositions/index.jsx
@@ -4,15 +4,14 @@ import { Controller, Navigation } from "swiper";
 import { DepositionCard } from "../commons/DepositionCard";
 import style from "./Depositions.module.css";
 import { data } from "../../utils/depositionCarouselInfo";
+import { Heading } from "../../components/commons/Heading";
 
 export function Depositions() {
   return (
     <>
-      <section className={style.DepositionsSection}>
+      <section className={style.DepositionsSection} id="depoimentos">
         <header className={style.depositionsHeader}>
-          <h2 className={style.depositionsTitle} id="depoimentos">
-            Depoimentos
-          </h2>
+          <Heading level={"h1"}>Depoimentos</Heading>
         </header>
         <div className={style.container}>
           <div className={style.wrapper}>
@@ -44,7 +43,8 @@ export function Depositions() {
                 rewind={true}
                 navigation={true}
                 controller={true}
-                modules={[Controller, Navigation]}>
+                modules={[Controller, Navigation]}
+              >
                 {data.map(({ imgPath, title, description, role, name }) => (
                   <SwiperSlide key={imgPath}>
                     <DepositionCard
