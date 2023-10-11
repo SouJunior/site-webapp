aa0dc239fcb125c22ac6c505226d7b6679ff41eb
Edvaldo Cabral <cabralnet@gmail.com>
2023-10-11 09:46:24 -0300

    add: Adicionando Google Adsense

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,14 @@
 import Script from "next/script";
 import "../styles/globals.css";
-import {Layout} from '../components/Layout'
-
+import { Layout } from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
       <Script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=G-L5P4NG9T9Y"></Script>
+        src="https://www.googletagmanager.com/gtag/js?id=G-L5P4NG9T9Y"
+      ></Script>
       <Script id="google-analytics" strategy="afterInteractive">
         {`
           window.dataLayer = window.dataLayer || [];
@@ -18,8 +18,12 @@ function MyApp({ Component, pageProps }) {
           gtag('config', 'G-L5P4NG9T9Y');
         `}
       </Script>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7597091185548852"
+        crossOrigin="anonymous"
+      ></Script>
       <Component {...pageProps} />
-      
     </Layout>
   );
 }
