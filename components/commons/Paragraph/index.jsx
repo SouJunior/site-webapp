e0715c24ad5162d3b1b2502eef00a3f42c48e3a3
Diego Fagundes <fagundesdiego2015bolcombr@gmail.com>
2025-05-08 17:11:11 -0300

    feat(parágrafo): adiciona link para "faça parte"
    Adiciona funcionalidade para transformar o texto "faça parte" dentro do componente de parágrafo em um link que aponta para a seção de participação da página.

--- components/commons/Paragraph/index.jsx
+++ components/commons/Paragraph/index.jsx
@@ -1,13 +1,39 @@
 import styles from "./Paragraph.module.css";
 
 export const Paragraph = ({ descripition, children }) => {
+  const convertTextToLink = (text) => {
+    if(typeof text !== 'string') return text;
+    if(!text.includes('em faça parte')) return text;
+
+    const regex = /(faça parte)/
+    const parts = text.split(regex);
+
+    if(parts.length === 1) return text;
+
+    return parts.map((part, index) => {
+      if(part.toLowerCase() === 'faça parte') {
+        return (
+          <a
+            key={index}
+            href="/?#participation"
+            rel="noopener noreferrer"
+            style={{fontWeight: "bold"}}
+          >
+            {part}
+          </a>
+        )
+      }
+      return part;
+    })
+  }
+
   return (
     <p
       className={styles.Paragraph}
       style={
         descripition ? { marginBlock: "1.5rem" } : { marginBlock: "0rem" }
       }>
-      {children}
+      {convertTextToLink(children)}
     </p>
   );
 };
